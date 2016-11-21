b21dc5262b8a839a1161078f15b4ea8d03800f6e
Maximilian Algehed <m.algehed@gmail.com>
2016-11-21 22:45:08 +0100

    Some comments

--- Port.hs
+++ Port.hs
@@ -3,8 +3,10 @@ module Port (Port(..), ParameterPort(..), IsPort(..)) where
 -- A port is just an address
 data Port a = Port Int
 
+-- A parameter port is a port with a default value
 data ParameterPort a = ParameterPort a Int
 
+-- Can I get a port ID
 class IsPort p where
     portID :: p a -> Int
 
