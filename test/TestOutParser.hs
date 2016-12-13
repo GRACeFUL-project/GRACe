306c1d3a50266eb15a2e3d3efc074fec7a66898c
Björn Norgren <bjonor@gmail.com>
2016-12-13 06:21:26 +0100

    OutParse test suite updated

--- test/TestOutParser.hs
+++ test/TestOutParser.hs
@@ -12,6 +12,7 @@ import OutParser
 main :: IO ()
 main = defaultMain $ tests
 
+-- | OutParser test group.
 tests :: TestTree
 tests = testGroup "OutParser tests"
   [ unitTests
@@ -19,55 +20,69 @@ tests = testGroup "OutParser tests"
   ]
 
 instance Arbitrary Value where
-  arbitrary = oneof [B <$> arbitrary, I <$> arbitrary, D <$> arbitrary]
+  arbitrary = oneof [B <$> arbitrary, N <$> arbitrary, D <$> arbitrary]
   shrink = \case
-    B b -> B <$> shrink b
-    I n -> I <$> shrink n
-    D d -> D <$> shrink d
+    B True -> [B False]
+    B False -> []
+    N 0 -> []
+    N n -> N <$> [n `div` 20, n `div` 2]
+    D d -> [D 0]
 
+instance Arbitrary Solution where
+  arbitrary = frequency [(5, Sol <$> sol), (1, OptSol <$> sol)]
+    where
+      sol = take 5 <$> listOf1 var
+      var = do
+        lbl <- listOf1 $ elements (['a'..'z'] ++ ['A'..'Z'] ++ "     ")
+        v <- arbitrary
+        return (take 35 lbl, v)
+  shrink = \case
+      Sol vars -> Sol <$> shrink vars
+      OptSol vars -> OptSol <$> shrink vars
 
 instance Arbitrary Output where
-  arbitrary = frequency [(10, sat), (1, unsat)]
-    where
-      sol = listOf1 var
-      str = listOf1 $ elements (['a'..'z'] ++ ['A'..'Z'] ++ "   ")
-      var = do
-        lbl <- str
-        v <- arbitrary
-        return (lbl, v)
-      sat = do
-        sols <- listOf1 sol
-        optSol <- frequency [(1, return (last sols)), (5, return [])]
-        return $ Sat sols optSol
-      unsat = Unsat <$> str
-      -- err = ParseErr <$> str -- Can't parse parse errors
+  arbitrary = Sat . take 5 <$> listOf1 arbitrary
+  shrink = \case
+    Sat sols -> map Sat $ shrink sols
+    Unsat _ -> undefined
+    ParseErr _ -> undefined
 
 ---- Unit tests ----------------------------------------------------------------
 
+-- | Unit test group
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
-  [ testCase "Single non-optimal result" unit_single
-  , testCase "Single optimal result" unit_single_opt
-  , testCase "Multiple sub-optimal results" unit_multiple
-  , testCase "Multiple sub-optimal results + one optimal" unit_multiple_opt
-  , testCase "Many results" unit_many
-  , testCase "Unsat result" unit_unsat
-  ]
+   [ testCase "Single non-optimal result" unit_single
+   , testCase "Single optimal result" unit_single_opt
+   , testCase "Multiple sub-optimal results" unit_multiple
+   , testCase "Multiple sub-optimal results + one optimal" unit_multiple_opt
+   , testCase "Many results" unit_many
+   , testCase "Unsat result" unit_unsat
+   ]
 
 -- | Test parsing a single non-optimal result.
 unit_single :: Assertion
 unit_single = expected @=? actual
   where
-    out = "a : 1\nb : 1\n----------"
-    expected = Sat [[("a", I 1), ("b", I 1)]] []
+    out = unlines
+      [ "a : 1"
+      , "b : 1"
+      , "----------"
+      ]
+    expected = Sat [Sol [("a", N 1), ("b", N 1)]]
     actual = par out
 
 -- | Test parsing a single optimal result.
 unit_single_opt :: Assertion
 unit_single_opt = expected @=? actual
   where
-    out = "a : 10\nb : 10\n----------\n=========="
-    expected = Sat [[("a",I 10),("b",I 10)]] [("a",I 10),("b",I 10)]
+    out = unlines
+      [ "a : 10"
+      , "b : 10"
+      , "----------"
+      , "=========="
+      ]
+    expected = Sat [OptSol [("a",N 10),("b",N 10)]]
     actual = par out
 
 -- | Test parsing multiple sub-optimal results.
@@ -86,11 +101,10 @@ unit_multiple = expected @=? actual
       , "----------"
       ]
     expected = Sat
-      [ [("a",I 1),("b",I 1)]
-      , [("a",I 2),("b",I 2)]
-      , [("a",I 3),("b",I 3)]
+      [ Sol [("a",N 1),("b",N 1)]
+      , Sol [("a",N 2),("b",N 2)]
+      , Sol [("a",N 3),("b",N 3)]
       ]
-      []
     actual = par out
 
 -- | Test parsing of multiple non-optimal results ending with an optimal result.
@@ -110,11 +124,10 @@ unit_multiple_opt = expected @=? actual
       , "=========="
       ]
     expected = Sat
-      [ [("a",I 1), ("b",I 1)]
-      , [("a",I 2), ("b",I 2)]
-      , [("a",I 3), ("b",I 3)]
+      [ Sol [("a",N 1), ("b",N 1)]
+      , Sol [("a",N 2), ("b",N 2)]
+      , OptSol [("a",N 3), ("b",N 3)]
       ]
-      [("a",I 3), ("b",I 3)]
     actual = par out
 
 -- | Test parsing of many results.
@@ -130,15 +143,14 @@ unit_many = expected @=? actual
       , "a : 10\nb : 3\nf : 13\ng : true\n----------\n=========="
       ]
     expected = Sat
-      [ [("a", I 10),("b", I 3),("f", I 4),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 5),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 6),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 7),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 8),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 9),("g", B True)]
-      , [("a", I 10),("b", I 3),("f", I 13),("g", B True)]
+      [ Sol [("a", N 10),("b", N 3),("f", N 4),("g", B True)]
+      , Sol [("a", N 10),("b", N 3),("f", N 5),("g", B True)]
+      , Sol [("a", N 10),("b", N 3),("f", N 6),("g", B True)]
+      , Sol [("a", N 10),("b", N 3),("f", N 7),("g", B True)]
+      , Sol [("a", N 10),("b", N 3),("f", N 8),("g", B True)]
+      , Sol [("a", N 10),("b", N 3),("f", N 9),("g", B True)]
+      , OptSol [("a", N 10),("b", N 3),("f", N 13),("g", B True)]
       ]
-      [("a",I 10),("b",I 3),("f",I 13),("g",B True)]
     actual = par out
 
 -- | Test parsing of unsat result.
@@ -157,22 +169,24 @@ unit_unsat = expected @=? actual
 
 ---- Property tests ------------------------------------------------------------
 
+-- | Property test group
 propTests :: TestTree
 propTests = testGroup "Property tests"
   [ testProperty "par . show == id" prop_show_par
   ]
 
+-- | par . show == id
 prop_show_par :: Output -> Bool
 prop_show_par out = par (show out) == out
 
 ex1 = unlines
-  [ "rTLn : -7.329885850880368"
-  , " Uf : 0"
-  , "YlE : true"
-  , "FA : 2"
+  [ "rTLn : 7.0"
+--  , " Uf : 0"
+--  , "YlE : true"
+--  , "FA : 2"
   , "----------"
-  , "O : false"
-  , "----------"
+--  , "O : false"
+--  , "----------"
   ]
 
 ex2 = unlines
@@ -186,6 +200,13 @@ ex2 = unlines
   , " Db : -3.122568798608161"
   , "glTj : -2.5730528796468577"
   , "k : 4.8498010947897345"
+  , "----------"
+  , "=========="
+  ]
+
+ex3 = unlines
+  [ "a : 1"
+  , "a : 1"
   , "----------"
   , "=========="
   ]
