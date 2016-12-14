b33152c0d6f6b80f86e70f0e859fccb947daa5d5
Björn Norgren <bjonor@gmail.com>
2016-12-14 22:15:32 +0100

    Add some GCMCheck tests

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -3,8 +3,10 @@ module Main where
 import Test.Tasty (defaultMain, testGroup)
 
 import TestOutParser (tests)
+import TestFW.TestGCMCheck (tests)
 
 main :: IO ()
 main = defaultMain $ testGroup "Test all"
   [ TestOutParser.tests
+  , TestFW.TestGCMCheck.tests
   ]
