e96721499dabed4f1d460a1d3cf90e7411864243
Björn Norgren <bjorn.norgren@fripost.org>
2016-12-14 12:41:08 +0100

    Create property run function

---
+++ src/TestFW/GCMCheck.hs
@@ -0,0 +1,14 @@
+module TestFW.GCMCheck where
+
+import qualified Test.QuickCheck as QC
+
+import Compile0
+import GCMP
+import OutParser
+
+-- | GCM property run function
+check :: GCMP a -> IO ()
+check prop = do
+  prg <- QC.generate $ makeGenerator prop
+  out <- runDef $ compileGCM prg
+  putStrLn $ show out
