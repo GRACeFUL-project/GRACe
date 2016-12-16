d344aa9518d838028013af35ca056341b7292124
Maximilian Algehed <m.algehed@gmail.com>
2016-12-16 10:01:01 +0100

    Some stuff

--- src/TestFW/GCMCheck.hs
+++ src/TestFW/GCMCheck.hs
@@ -27,7 +27,7 @@ checkOnce :: GCMP a -> IO (Maybe Solution)
 checkOnce prop = do
   prg <- QC.generate $ makeGenerator prop
   out <- runDef $ compileGCM prg
-  print out
+  print (map verify out)
   return $ case out of
     Sat sols -> msum $ map verify sols
     _ -> Nothing
