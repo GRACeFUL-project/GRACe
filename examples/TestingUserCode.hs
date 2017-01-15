229b9283adf01513cdd4eea6e996d3fb0b5e3bed
Maximilian Algehed <m.algehed@gmail.com>
2017-01-15 19:36:37 +0100

    Started hacking on the code that does the property based testing

---
+++ examples/TestingUserCode.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE TypeApplications #-}
+import Control.Monad
+import qualified Data.HashMap.Lazy as HM
+import System.Process
+import TestFW.GCMCheck
+import Compile0
+import GL
+import TestFW.GCMP
+import qualified Test.QuickCheck as QC
+import Data.List.Split
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as BS
+import Data.Maybe
+import qualified Data.Text as T
+import Data.List
+
+data Pump = Pump {inflow :: Port Int, outflow :: Port Int, capacity :: Param Int}
+
+-- A pump with a initial capacity c
+pump :: Int -> GCM Pump
+pump c = do
+  iflow <- createPort
+  oflow <- createPort
+  cap  <- createParam c
+  component $ do
+    ifl <- value iflow
+    ofl <- value oflow
+    c <- value cap
+    assert $ ifl `inRange` (0, c)
+    assert $ ofl === ifl
+  return (Pump iflow oflow cap)
+
+prop_pump :: GCMP ()
+prop_pump = do
+  k <- forall (fmap abs QC.arbitrary)
+
+  pmp <- liftGCM $ pump k
+
+  property $ val (inflow pmp) .< lit k
+
+-- | Runs a prog expected to contain "prop_..." output
+-- and tests if that output contained any "prop_... = false"
+run :: GCM a -> IO Bool
+run prog = do
+  writeFile "model.mzn" (compileGCM prog)
+  callCommand "mzn2fzn model.mzn"
+  out <- readCreateProcess (shell "fzn-gecode -p 4 -n 10 model.fzn | solns2out --soln-sep \"\" --search-complete-msg \"\" model.ozn") ""
+  callCommand "rm model.mzn model.ozn model.fzn"
+  return (wasSucess out)
+
+-- | Tests that all label starting with "prop_" in MiniZinc output are True
+wasSucess :: String -> Bool
+wasSucess s =
+  let keyValue = [HM.toList hm
+                 | Object hm <-
+                    map fromJust $
+                    filter isJust $
+                    map ((decode @Value) . BS.pack) $
+                    filter (not . null) $
+                    splitOn "\n\n" s
+                ]
+      bools = [b | (label, Bool b) <- (concat keyValue), "prop_" `isPrefixOf` (T.unpack label)]
+  in  and bools
+
+test :: GCMP a -> IO ()
+test prop = do
+  let generator = makeGenerator prop 
+  results <- replicateM 10 $ QC.generate generator >>= run
+  if and results then
+    putStrLn "Success"
+  else
+    putStrLn "Failure"
+
+main = test prop_pump
