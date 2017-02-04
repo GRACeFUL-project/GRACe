e930ebba117fc14f31465a17f50ab260f1a10241
Maximilian Algehed <m.algehed@gmail.com>
2017-02-04 23:25:35 +0100

    Started cleaning up the JSON interface for the Graph datatype

--- execsrc/Graph2GRACe.hs
+++ execsrc/Graph2GRACe.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, TypeApplications #-}
 import System.Environment
 import Data.Aeson
 import Data.Aeson.Types
@@ -13,7 +13,7 @@ import qualified Data.Text as T
 {-
  - TODO
  -  * Hand-craft a nice JSON format.
- -  * Factor out the graph representation,
+ -  * Factor out the graph representation from this file,
  -    we should use it with as many Maybe etc.
  -    as possible to make sure we only have _one_
  -    representation present in both Lib2JSON and
@@ -41,34 +41,44 @@ data Node = Node { identity   :: Maybe Int   -- * What ID does this node have?
                  }
   deriving (Generic, Show, Eq)
 
+nodeOptions :: Options
+nodeOptions = defaultOptions {omitNothingFields = True}
+
 -- | Obvious instances
 instance ToJSON Node where
-  toEncoding = genericToEncoding $ defaultOptions {omitNothingFields = True}
+  toEncoding = genericToEncoding nodeOptions
 
 -- | Obvious instances
 instance FromJSON Node where
-  parseJSON  = genericParseJSON  $ defaultOptions {omitNothingFields = True}
+  parseJSON  = genericParseJSON  nodeOptions
 
--- | Fix the prefixes
 data Parameter = Parameter { parameterName  :: String
                            , parameterType  :: PrimType
                            , parameterValue :: PrimTypeValue
                            }
   deriving (Generic, Show, Eq)
 
-instance ToJSON Parameter where
-  toEncoding = genericToEncoding defaultOptions
+parameterOptions :: Options
+parameterOptions = defaultOptions { fieldLabelModifier = (map toLower) . (drop $ length ("parameter" :: String)) }
 
-instance FromJSON Parameter
+instance ToJSON Parameter where
+  toEncoding = genericToEncoding parameterOptions
+
+instance FromJSON Parameter where
+  parseJSON  = genericParseJSON  parameterOptions
 
 -- | The primitive types in `GRACe`
 data PrimType = FloatT | IntT | StringT | BoolT
   deriving (Generic, Show, Eq)
 
-instance ToJSON PrimType where
-  toEncoding = genericToEncoding defaultOptions
+primTypeOptions :: Options
+primTypeOptions = defaultOptions { constructorTagModifier = init }
 
-instance FromJSON PrimType
+instance ToJSON PrimType where
+  toEncoding = genericToEncoding primTypeOptions
+
+instance FromJSON PrimType where
+  parseJSON  = genericParseJSON  primTypeOptions
 
 data PrimTypeValue = FloatV Float | IntV Int | StringV String | BoolV Bool
   deriving (Generic, Show, Eq)
@@ -88,17 +98,20 @@ instance FromJSON PrimTypeValue where
   parseJSON _          = fail "Does not comform to interface"
 
 -- | Fix the prefixes
-data Interface = Interface { portName :: String
-                           , portType :: String
-                           , portConn :: Maybe (Int, String)
+data Interface = Interface { interfaceName       :: String
+                           , interfaceType       :: String
+                           , interfaceConnection :: Maybe (Int, String)
                            }
   deriving (Generic, Show, Eq)
 
+interfaceOptions :: Options
+interfaceOptions = defaultOptions { fieldLabelModifier = (map toLower) . (drop (length ("interface" :: String))), omitNothingFields = True }
+
 instance ToJSON Interface where
-  toEncoding = genericToEncoding $ defaultOptions {omitNothingFields = True}
+  toEncoding = genericToEncoding interfaceOptions
 
 instance FromJSON Interface where
-  parseJSON  = genericParseJSON $ defaultOptions {omitNothingFields = True}
+  parseJSON  = genericParseJSON  interfaceOptions
 
 example = Graph
   [ Node 
@@ -142,22 +155,22 @@ prettyArgs :: Node -> [String]
 prettyArgs n = [pPrintPTV (parameterValue p) | p <- parameters n]
 
 allPortNames :: Graph -> [String]
-allPortNames g = concatMap portNames (nodes g)
+allPortNames g = concatMap interfaceNames (nodes g)
 
-portNames :: Node -> [String]
-portNames n = [ "id" ++ show (fromMaybe 0 (identity n)) ++ portName p | p <- interface n]
+interfaceNames :: Node -> [String]
+interfaceNames n = [ "id" ++ show (fromMaybe 0 (identity n)) ++ interfaceName p | p <- interface n]
 
 allPortLinks :: Graph -> [String]
 allPortLinks g = concatMap portLinks (nodes g)
 
 portLinks :: Node -> [String]
-portLinks n = [ "link id" ++ (show (fromMaybe 0 (identity n))) ++ portName p ++ " id" ++ (show i) ++ s
+portLinks n = [ "link id" ++ (show (fromMaybe 0 (identity n))) ++ interfaceName p ++ " id" ++ (show i) ++ s
               | p <- interface n
-              , isJust (portConn p)
-              , let (i, s) = fromJust (portConn p)]
+              , isJust (interfaceConnection p)
+              , let (i, s) = fromJust (interfaceConnection p)]
 
 bindVariables :: Graph -> [String]
-bindVariables g = ["(" ++ intercalate "," (portNames n) ++ ")" | n <- nodes g]
+bindVariables g = ["(" ++ intercalate "," (interfaceNames n) ++ ")" | n <- nodes g]
 
 nodeInitialisations :: Graph -> [String]
 nodeInitialisations g = zipWith (\l r -> l ++ "<-" ++ r) (bindVariables g) (components g)
