1f90d34e98436256d856871cd6b39ad20beca017
Alex Gerdes <agerdes@me.com>
2017-03-23 13:57:45 +0100

    Some unit tests for the web services.

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -1,12 +1,43 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Test.Tasty (defaultMain, testGroup)
+
+import Control.Concurrent (threadDelay)
+import Control.Exception (catch)
+import System.Exit (ExitCode)
+import Test.Tasty (defaultMain, testGroup, TestTree)
+import Test.Tasty.HUnit
+import System.Process
 
 import TestOutParser (tests)
 import TestFW.TestGCMCheck (tests)
 
 main :: IO ()
-main = defaultMain $ testGroup "Test all"
-  [ TestOutParser.tests
-  , TestFW.TestGCMCheck.tests
+main = do
+    -- Setup
+    server <- spawnCommand "stack exec RestAPI"
+    threadDelay 1000000  -- wait a second for the server to get started
+
+    -- Test
+    defaultMain (testGroup "Test all"
+      [ TestOutParser.tests
+      , TestFW.TestGCMCheck.tests
+      , serviceTests
+      ]) `catch` \(e :: ExitCode) -> return ()
+
+    -- Teardown
+    terminateProcess server
+
+serviceTests = testGroup "Unit tests" 
+  [ testService "library/crud" [] "test/test_library.exp"
+  , testService "submit" [ "-H", "Content-Type: application/json"
+                         , "--data", "@example.json"] "test/test_submit.exp"
   ]
+
+testService :: String -> [String] -> FilePath -> TestTree
+testService endpoint options file = testCase ("Testing: " ++ file) $ do
+    out <- readProcess "curl" (options ++ ["-s", url]) "" 
+    exp <- readFile file
+    out @?= exp
+  where
+    url = "http://localhost:8081/" ++ endpoint 
