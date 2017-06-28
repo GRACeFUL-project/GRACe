130b93e3e85ba9c61ddfba1b60727856a6c21381
solrun <solalola@gmail.com>
2017-06-28 17:44:09 +0200

    Prettified oil example

--- examples/OilCrops.hs
+++ examples/OilCrops.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE RecordWildCards       #-}
--- The first part of the biodiesel example, but with integers instead of floats.
 module Main where
 
 import Compile0 (runGCM)
@@ -11,7 +10,6 @@ import GCM      ( GCM, output, component
                 )
 import CP       ( CPType, assert, lit, max', (===), (.<=), inRange, CPExp)
 
-{-- * TODO: Needs a problem description at some point -}
 
 -- * Vegetable oil manufacturing
 -------------------------------------------------------------------------------
@@ -19,6 +17,7 @@ import CP       ( CPType, assert, lit, max', (===), (.<=), inRange, CPExp)
 -- | There are three crops which provide us with vegetable oil:
 --
 -- Soybeans, sunflower seeds and cotton seeds.
+
 data Crop = Soy | Sunflower | Cotton
 
 type Area  = Int
@@ -28,17 +27,17 @@ type Oil   = Int
 
 -- | Crop yield/water demand table:
 --
---   Crop            Yield [t/ha]   Water demand [Ml/ha]  Oil content [l/kg]
+--   Crop            Yield [t/ha]   Water demand [Ml/ha]  Oil content [l/t]
 --   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---   Soybeans        2.5            5.0                   0.178
---   Sunflower seeds 1.5            4.2                   0.216
---   Cotton seeds    0.8            1.0                   0.433
+--   Soybeans         3              5                     178
+--   Sunflower seeds  2              4                     216
+--   Cotton seeds     1              1                     433
 --
 -- Note the units.
 cropTable :: Crop -> (Yield, Water, Oil)
-cropTable Soy       = (3, 5, 1)
-cropTable Sunflower = (2, 4, 2)
-cropTable Cotton    = (1, 1, 4)
+cropTable Soy       = (3, 5, 178)
+cropTable Sunflower = (2, 4, 216)
+cropTable Cotton    = (1, 1, 433)
 
 -- | GCM component for a single crop.
 --
@@ -50,8 +49,12 @@ cropTable Cotton    = (1, 1, 4)
 cropToOil :: Crop -> GCM (Port Area, Port Oil, Port Water)
 cropToOil crop = do
   -- Declare ports.
+
+  -- Area (in ha) used to grow crop.
   areaPort  <- createPort
+  -- Amount of oil produced from crop.
   oilPort   <- createPort
+  -- Amount of water used by crop.
   waterPort <- createPort
 
   -- Constrain the values at the ports.
@@ -60,33 +63,25 @@ cropToOil crop = do
     oilValue   <- value oilPort
     waterValue <- value waterPort
 
-    -- Get tabulated values for crop.
+    -- Get tabulated values for the crop.
     let (y, w, o) = cropTable crop
 
-    -- For some reason, yield is given as tonnes and oil in litres per kg.
-    assert $ oilValue   === 1000 * lit y * lit o * areaValue
+    -- Calculate values from data.
+    assert $ oilValue   === lit y * lit o * areaValue
     assert $ waterValue === lit w * areaValue
 
   return (areaPort, oilPort, waterPort)
 
--- | GCM component for optimizing crop production to provide the maximum benefit
--- for our Biodiesel production. The following quantities are available:
---
---   Farmland: 1,600 ha
---   Water:    5,000 Ml
---
--- (Although implicit, this just amounts to maximizing vegetable oil yield given
--- the available farmland and water.)
-oilProduction :: GCM (Port Oil, Port Water, Port Area, Port Area, Port Area)
-oilProduction = do
-  let farmland = 1600
-      water    = 5000
+-- | GCM component for optimizing crop production to provide the maximum amount
+--   of oil for the available land and water.
+oilProduction :: Area -> Water -> GCM (Port Oil, Port Water, Port Area, Port Area, Port Area)
+oilProduction farmland water = do
 
   -- Port for total vegetable oil yield.
   oilYieldPort <- createPort
-
+  -- Port for amount of water used.
   waterPort <- createPort
-
+  -- Ports for area used to grow each crop.
   soyPort  <- createPort
   sunfPort <- createPort
   cottPort <- createPort
@@ -94,47 +89,59 @@ oilProduction = do
   -- Get the relevant ports for all crops.
   cropPorts <- mapM cropToOil [Soy, Sunflower, Cotton]
 
-  -- 1. The total area of crops is non-negative and is bounded by the available
-  --    farmland.
-  -- 2. The total amount of water used is non-negative and is bounded by the
-  --    available water reservoir.
   component $ do
     -- Values for crops: (area, water, oil)
-    areas  <- sequence [ value ap | (ap, _, _) <- cropPorts ]
+    areas@[soyArea, sunfArea, cottArea]  <-
+      sequence [ value ap | (ap, _, _) <- cropPorts ]
     waters <- sequence [ value wp | (_, _, wp) <- cropPorts ]
     oils   <- sequence [ value op | (_, op, _) <- cropPorts ]
+
     yield  <- value oilYieldPort
     watertotal <- value waterPort
     soy <- value soyPort
     sunf <- value sunfPort
     cott <- value cottPort
 
+    --  The total area of crops is non-negative and is bounded by the available
+    --  farmland. Each crop area is also non-negative.
     assert $ sum areas `inRange` (0, lit farmland)
+    mapM_ (\x -> assert $ 0 .<= x) areas
+    -- The total amount of water used is non-negative and is bounded by the
+    -- available water reservoir. The amount for each crop is also non-negative.
     assert $ sum waters `inRange` (0, lit water)
-    assert $ sum oils === yield
-    assert $ sum waters === watertotal
-    assert $ head areas === soy
-    assert $ 0 .<= soy
-    assert $ (head $ tail areas) === sunf
-    assert $ 0 .<= sunf
-    assert $ last areas === cott
-    assert $ 0 .<= cott
+    mapM_ (\x -> assert $ 0 .<= x) waters
+
+    -- We constrain the values we are interested in to our ports.
+    assert $ yield === sum oils
+    assert $ watertotal === sum waters
+    assert $ soy === soyArea
+    assert $ sunf === sunfArea
+    assert $ cott === cottArea
 
   return (oilYieldPort, waterPort, soyPort, sunfPort, cottPort)
 
+-- | Help function for maximizing goal.
 maximize :: Port Int -> GCM ()
 maximize p = do
   g <- createGoal
   link p g
 
+-- | In this example the following quantities are available:
+--
+--   Farmland: 1,600 ha
+--   Water:    5,000 Ml
+--
 problem :: GCM ()
 problem = do
   -- Create system
-  (oilProd, watertotal, soy, sun, cot) <- oilProduction
+  (oil, water, soy, sun, cot) <- oilProduction 1600 5000
 
-  maximize oilProd
-  output oilProd "Oil produced"
-  output watertotal "Water used"
+  -- Our goal is to maximize the amount of oil produced.
+  maximize oil
+
+  -- We print the values we would like to see.
+  output oil "Oil produced"
+  output water "Water used"
   output soy "Soybean area"
   output sun "Sunflower area"
   output cot "Cotton area"
