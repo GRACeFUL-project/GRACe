e6787d48a733af0f818fa7c1ec3f51164ed8442f
Maximilian Algehed <m.algehed@gmail.com>
2017-07-27 11:54:37 +0200

    CLD with pArrow, mArrow, and combine

---
+++ examples/CLD.hs
@@ -0,0 +1,35 @@
+module CLD where
+import GCM
+import CP
+
+type S = Int
+
+pArrow :: GCM (Port S, Port S)
+pArrow = do
+  p <- createPort
+  q <- createPort
+  link p q
+  return (p, q)
+
+mArrow :: GCM (Port S, Port S)
+mArrow = do
+  p <- createPort
+  q <- createPort
+  component $ do
+    p <- value p
+    q <- value q
+    assert $ q === (-1) * p
+  return (p, q)
+
+combine :: GCM (Port S, Port S, Port S)
+combine = do
+  i0 <- createPort
+  i1 <- createPort
+  o  <- createPort
+  component $ do
+    o  <- value o
+    i0 <- value i0
+    i1 <- value i1
+    assert $ (i0 === i1) ==> (o === i1)
+    assert $ (i0 /== i1) ==> (o === 0 )
+  return (i0, i1, o)
