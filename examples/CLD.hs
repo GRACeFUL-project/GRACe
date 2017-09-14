1529d8b8caa4c777b97fe5d9da4ffbebeef3fdc8
solrun <solalola@gmail.com>
2017-09-15 00:12:40 +0200

    new cld model, works but super slow

--- examples/CLD.hs
+++ examples/CLD.hs
@@ -1,8 +1,41 @@
 module CLD where
 import GCM
 import CP
+import Compile(runCompare)
 
 type S = Int
+
+plus :: (Num a) => a
+plus = 1
+
+minus :: (Num a) => a
+minus = -1
+
+zero :: (Num a) => a
+zero = 0
+
+ambig :: (Num a) => a
+ambig = 1000
+
+add :: Port S -> CPExp S -> CPExp S -> CP ()
+add p x y = do
+  vp <- value p
+  assert $ (x === y) ==> (vp === x)
+  assert $ ((x /== y) .&& (x /== 0) .&& (y /== 0)) ==> (vp === ambig)
+  assert $ ((x === 0) .|| (y === 0)) ==> (vp === (x + y))
+
+constructSum :: [Port S] -> GCM (Port S)
+constructSum [] = do
+  p <- createPort
+  set p 0
+  return p
+constructSum (x:xs) = do
+  hereResult <- createPort
+  restResult <- constructSum xs
+  vx <- value x
+  vr <- value restResult
+  component $ add hereResult vx vr
+  return hereResult
 
 pArrow :: GCM (Port S, Port S)
 pArrow = do
@@ -16,9 +49,9 @@ mArrow = do
   p <- createPort
   q <- createPort
   component $ do
-    p <- value p
-    q <- value q
-    assert $ q === (-1) * p
+    pv <- value p
+    qv <- value q
+    assert $ qv === (-1) * pv
   return (p, q)
 
 (-+>) :: Port S -> Port S -> GCM ()
@@ -33,15 +66,56 @@ p --> q = do
  link p p0
  link q q0
 
-combine :: GCM (Port S, Port S, Port S)
-combine = do
-  i0 <- createPort
-  i1 <- createPort
-  o  <- createPort
+cldNode :: S -> Int -> GCM ([Port S], Port S)
+cldNode obsSign n = do
+  inPorts <- mapM (\_ -> createPort) (take n (repeat 0))
+  influence <- constructSum inPorts
+  outPort <- createPort
   component $ do
-    o  <- value o
-    i0 <- value i0
-    i1 <- value i1
-    assert $ (i0 === i1) ==> (o === i1)
-    assert $ (i0 /== i1) ==> (o === 0 )
-  return (i0, i1, o)
+    i <- value influence
+    o <- value outPort
+    assert $ (i === 0) ==> (o === lit obsSign)
+    assert $ ((lit obsSign) === 0) ==> (o === i)
+    assert $ ((i /== 0) .&& ((lit obsSign) /== 0)) ==> (o === (i * (lit obsSign)))
+  return (inPorts, outPort)
+
+tinyExample :: GCM ()
+tinyExample = do
+  ([], a) <- cldNode plus 0
+
+  ([], b) <- cldNode minus 0
+
+  ([c1,c2], c) <- cldNode zero 2
+
+  a --> c1
+  b -+> c2
+  output a "a"
+  output b "b"
+  output c "c"
+
+drudzelHenrion :: GCM ()
+drudzelHenrion = do
+  ([a1], a ) <- cldNode plus 1
+  ([], b) <- cldNode zero 0
+  ([c1], c) <- cldNode zero 1
+  ([d1,d2], d) <- cldNode zero 2
+  ([e1,e2], e) <- cldNode zero 2
+  ([], f) <- cldNode zero 0
+  b -+> a1
+  b -+> c1
+  b -+> d1
+  c -+> d2
+  d --> e1
+  f --> e2
+
+  output a "a"
+  output b "b"
+  output c "c"
+  output d "d"
+  output e "e"
+  output f "f"
+
+main :: IO ()
+main = do
+  runCompare tinyExample
+  runCompare drudzelHenrion
