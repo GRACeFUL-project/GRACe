cd81621e5bc852931ff2f4212481cb7f52b2d228
solrun <solalola@gmail.com>
2017-09-17 22:14:04 +0200

    Some more CLDexamples

--- examples/CLD.hs
+++ examples/CLD.hs
@@ -9,7 +9,7 @@ add p x y = do
   vp <- value p
   assert $ (x === y) ==> (vp === x)
   assert $ ((x /== y) .&& (x /== Lit Z) .&& (y /== Lit Z)) ==> (vp === Lit Q)
-  assert $ ((x === Lit Z) .|| (y === Lit Z)) ==> (vp === (x + y)) 
+  assert $ ((x === Lit Z) .|| (y === Lit Z)) ==> (vp === (x + y))
 
 constructSum :: [Port Sign] -> GCM (Port Sign)
 constructSum [] = do
@@ -101,6 +101,36 @@ tinyExample2 = do
   output c "c"
   output d "d"
 
+-- Loops
+positiveLoop :: GCM ()
+positiveLoop = do
+  ([a1],a) <- cldNode (Just M) 1
+  ([b1],b) <- cldNode Nothing 1
+  a -+> b1
+  b -+> a1
+  output a "a"
+  output b "b"
+
+posNegLoop :: GCM ()
+posNegLoop = do
+  ([a1],a) <- cldNode (Just M) 1
+  ([b1],b) <- cldNode Nothing 1
+  a --> b1
+  b --> a1
+  output a "a"
+  output b "b"
+
+-- | Negative feedback loop. The solver returns UNSAT.
+negativeLoop :: GCM ()
+negativeLoop = do
+  ([a1],a) <- cldNode (Just M) 1
+  ([b1],b) <- cldNode Nothing 1
+  a -+> b1
+  b --> a1
+  output a "a"
+  output b "b"
+
+-- | Example CLD from Druzdel & Henrion 1993
 drudzelHenrion :: GCM ()
 drudzelHenrion = do
   ([a1], a ) <- cldNode (Just P) 1
@@ -123,11 +153,133 @@ drudzelHenrion = do
   output e "e"
   output f "f"
 
+-- | Example CLD from p.51, Figure 3.6, of Van Kouwen’s Ph.D. thesis, The Quasta Approach
+vanKouwen1 :: GCM ()
+vanKouwen1 = do
+  ([], a) <- cldNode Nothing 0
+  ([b1,b2,b3], b) <- cldNode Nothing 3
+  ([], c) <- cldNode Nothing 0
+  ([d1,d2], d) <- cldNode Nothing 2
+  ([], e) <- cldNode Nothing 0
+  ([f1], f) <- cldNode Nothing 1
+  ([g1,g2],g) <- cldNode (Just P) 2
+  a -+> b2
+  e -+> b1
+  e -+> f1
+  f -+> g1
+  b -+> g2
+  c --> b3
+  c -+> d2
+  b -+> d1
+
+  output a "a"
+  output b "b"
+  output c "c"
+  output d "d"
+  output e "e"
+  output f "f"
+  output g "g"
+
+-- | Example CLD from p.52, Figure 3.8, of Van Kouwen’s Ph.D. thesis, The Quasta Approach
+vanKouwen2 :: GCM ()
+vanKouwen2 = do
+  ([],a) <- cldNode Nothing 0
+  ([b1,b2,b3],b) <- cldNode Nothing 3
+  ([],c) <- cldNode Nothing 0
+  ([d1,d2],d) <- cldNode (Just P) 2
+  ([],e) <- cldNode Nothing 0
+  ([f1],f) <- cldNode Nothing 1
+  ([g1,g2],g) <- cldNode Nothing 2
+
+  e -+> f1
+  f -+> g1
+  e -+> b1
+  a -+> b2
+  b -+> g2
+  c --> b3
+  b -+> d1
+  c -+> d2
+
+  output a "a"
+  output b "b"
+  output c "c"
+  output d "d"
+  output e "e"
+  output f "f"
+  output g "g"
+
+-- | Example CLD from p.68, Figure 4.5, of Van Kouwen’s Ph.D. thesis, The Quasta Approach
+--   Contains negative feedback loop, solver says UNSAT.
+vanKouwen3 :: GCM ()
+vanKouwen3 = do
+  ([],a) <- cldNode Nothing 0       -- Sea level rise
+  ([b1,b2],b) <- cldNode Nothing 2  -- Risk of flooding
+  ([c1],c) <- cldNode Nothing 1     -- Flooding
+  ([d1,d2], d) <- cldNode Nothing 2 -- Measures to prevent flooding
+  ([e1],e) <- cldNode (Just P) 1    -- Ecology in coastal zone
+  ([], f) <- cldNode Nothing 0      -- Investments
+
+  a -+> b1
+  b -+> c1
+  c -+> d1
+  d --> b2
+  d --> e1
+  f -+> d2
+
+  output a "a"
+  output b "b"
+  output c "c"
+  output d "d"
+  output e "e"
+  output f "f"
+
+-- | Example CLD from p.71, Figure 4.8, of Van Kouwen’s Ph.D. thesis, The Quasta Approach.
+vanKouwen4 :: GCM ()
+vanKouwen4 = do
+  ([a1,a2],a) <- cldNode (Just M) 2 --Turbidity
+  ([b1,b2],b) <- cldNode Nothing 2 -- Resuspended sedimentation
+  ([c1],c) <- cldNode Nothing 1 -- Algae
+  ([],d) <- cldNode Nothing 0 -- Water depth
+  ([e1,e2,e3],e) <- cldNode Nothing 3 -- Nutrients
+  ([f1,f2],f) <- cldNode Nothing 2 -- Vegetation
+  ([g1],g) <- cldNode Nothing 1 -- Waves
+  ([h1],h) <- cldNode Nothing 1 -- Allelopathic subs.
+  ([],i) <- cldNode Nothing 0 -- Fish
+  ([k1,k2],k) <- cldNode Nothing 2 -- Zooplankton
+
+  b -+> a1
+  c -+> a2
+  g -+> b1
+  i -+> b2
+  e -+> c1
+  h --> e1
+  f --> e2
+  k --> e3
+  a --> f1
+  d --> f2
+  f --> g1
+  f -+> h1
+  f -+> k1
+  i --> k2
+
+  output a "a"
+  output b "b"
+  output c "c"
+  output d "d"
+  output e "e"
+  output f "f"
+  output g "g"
+  output h "h"
+  output i "i"
+  output k "k"
+
 main :: IO ()
 main = do
   --runCompare tinyExample
   --compileString drudzelHenrion
-  runCompare drudzelHenrion
-
-  --compileString tinyExample2
-  runCompare tinyExample2
+  --runCompare drudzelHenrion
+  runCompare vanKouwen4
+  --runCompare tinyExample2
+  --runCompare positiveLoop
+  --runCompare posNegLoop
+  runCompare negativeLoop
